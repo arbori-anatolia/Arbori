3685aa46051117181fab77d92082a4bc7814fd6c
ark <tr4pperrapper@gmail.com>
2024-01-03 13:01:33 +0200

    nomizw dulevei

--- app/index.tsx
+++ app/index.tsx
@@ -6,7 +6,7 @@ export default function MainPage() {
   return (
     <View style={styles.container}>
       <Text>This is the starting page</Text>
-      <Link href="/home/index.tsx" asChild style={{ marginTop: 30 }}>
+      <Link href="home" asChild style={{ marginTop: 30 }}>
         <Pressable style={{ backgroundColor: 'rgba(255,255,255,0.90)', borderRadius: 9 }}>
             <Text style={{ padding: 16, fontSize: 18, color: 'black' }}>Go to home!</Text>
         </Pressable>
