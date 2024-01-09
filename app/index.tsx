e5a1b9e482c0762112c796d077043d2870f302e0
Ioannakaratziovali <iokarliou@gmail.com>
2024-01-09 13:35:00 +0200

    new landing page

--- app/index.tsx
+++ app/index.tsx
@@ -1,26 +1,73 @@
-import { StatusBar } from 'expo-status-bar';
-import { Pressable, StyleSheet, Text, View } from 'react-native';
-import { Link } from 'expo-router';
+import React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-export default function MainPage() {
+export default function HomePage() {
   return (
     <View style={styles.container}>
-      <Text>This is the starting page</Text>
-      <Link href="home" asChild style={{ marginTop: 30 }}>
-        <Pressable style={{ backgroundColor: 'rgba(255,255,255,0.90)', borderRadius: 9 }}>
-            <Text style={{ padding: 16, fontSize: 18, color: 'black' }}>Go to home!</Text>
-        </Pressable>
-    </Link>
-      <StatusBar style="auto" />
+      <Image
+        source={require('../assets/images/plant.jpeg')}
+        style={styles.backgroundImage}
+      />
+      <View style={styles.overlay}>
+        <Text style={styles.headerText}>Manage your Aerogarden</Text>
+        <View style={styles.menuContainer}>
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuText}>My Plants</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuText}>Explore</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.menuItem}>
+            <Text style={styles.menuText}>My Account</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: '#AECBA4',
+  },
+  backgroundImage: {
+    flex: 1,
+    resizeMode: 'cover', 
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
+  },
+  overlay: {
+    flex: 1,
+    backgroundColor: 'rgba(174,203,164,0.8)', 
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 20,
+  },
+  menuContainer: {
+    width: '80%',
+    marginBottom: 20,
+  },
+  menuItem: {
+    padding: 20,
+    marginVertical: 10,
+    backgroundColor: '#4CAF50', 
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  menuText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
   },
 });
+
+export default HomePage;
