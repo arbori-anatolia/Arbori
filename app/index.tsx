269895b7afc085eedf542983e9f7dfe15de53832
ark <tr4pperrapper@gmail.com>
2024-01-09 13:38:20 +0200

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 
-export default function HomePage() {
+const HomePage = () => {
   return (
     <View style={styles.container}>
       <Image
